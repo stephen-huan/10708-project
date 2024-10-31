560324cdd48f9e73d37264e1f5a2ea1fad6239e2
Stephen Huan <stephen.huan@cgdct.moe>
2024-10-30 21:51:15 -0700

    chore(elliptic/__init__.py): expose solvers

--- elliptic/__init__.py
+++ elliptic/__init__.py
@@ -1,7 +1,10 @@
 from .monte_carlo_matrix import monte_carlo_matrix_inversion
 from .monte_carlo_pde import monte_carlo_pde_solution
+from .utils import gauss_seidel, jacobi
 
 __all__ = [
+    "gauss_seidel",
+    "jacobi",
     "monte_carlo_matrix_inversion",
     "monte_carlo_pde_solution",
 ]
