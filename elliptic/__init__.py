dbcd4ea1a0a4e528a0077e261f1ccb574f018b93
Stephen Huan <stephen.huan@cgdct.moe>
2024-12-06 12:52:32 -0800

    refactor(elliptic): reorganize

--- elliptic/__init__.py
+++ elliptic/__init__.py
@@ -1,6 +1,6 @@
 from .monte_carlo_matrix import monte_carlo_matrix_inversion
 from .monte_carlo_pde import monte_carlo_pde_solution
-from .utils import gauss_seidel, jacobi
+from .solvers import gauss_seidel, jacobi
 from .WoS import walk_on_spheres
 
 __all__ = [
