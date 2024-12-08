dad98711259ef90b7137b3105bc8d39e51c60823
Stephen Huan <stephen.huan@cgdct.moe>
2024-12-08 11:44:59 -0800

    feat(elliptic/problems): add model problems

--- elliptic/problems.py
+++ elliptic/problems.py
@@ -67,7 +67,7 @@ def initialize_solution_rectangle(
     return solution
 
 
-# @partial(jit, static_argnums=(0, 1))
+@partial(jit, static_argnums=(0, 1))
 def initialize_solution_ushape(
     nx: int, ny: int, boundaries: Array | None = None
 ) -> tuple[Array, Array]:
@@ -202,3 +202,29 @@ def boundary_function(boundaries: Array) -> Callable[[Array], Array]:
         return boundaries[index[j]]
 
     return Partial(g)
+
+
+problems = [
+    (
+        initialize_solution_square(100, boundaries=jnp.array([0, 0, 100, 0])),
+        "square",
+    ),
+    (
+        initialize_solution_rectangle(
+            100, 10, boundaries=jnp.array([0, 50, 100, 0])
+        ),
+        "rectangle",
+    ),
+    (
+        initialize_solution_circle(
+            100, 100, boundaries=jnp.array([0, 100]), eps=55
+        ),
+        "circle",
+    ),
+    (
+        initialize_solution_ushape(
+            100, 100, boundaries=jnp.array([0, 50, 100, 0])
+        ),
+        "ushape",
+    ),
+]
