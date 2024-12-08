8910e4c22501ff1976c6722c1d2712d63434a82a
Stephen Huan <stephen.huan@cgdct.moe>
2024-12-08 12:21:32 -0800

    feat(elliptic/plotting): add dolfin plotting

--- elliptic/plotting.py
+++ elliptic/plotting.py
@@ -1,7 +1,24 @@
 from pathlib import Path
+from typing import Any
 
+import dolfin
+import matplotlib.pyplot as plt
 import seaborn as sns
 from jax import Array
+
+
+def plot_dolfin(A: Any, path: Path | str | None = None) -> None:
+    """Wrap dolfin's plot."""
+    dolfin.plot(A)
+    ax = plt.gca()
+    ax.set_frame_on(False)
+    ax.tick_params(
+        left=False, bottom=False, labelleft=False, labelbottom=False
+    )
+    plt.tight_layout()
+    if path is not None:
+        plt.savefig(path, pad_inches=0)
+    plt.gcf().clear()
 
 
 def plot_matrix(A: Array, path: Path | str | None = None) -> None:
@@ -11,8 +28,11 @@ def plot_matrix(A: Array, path: Path | str | None = None) -> None:
         xticklabels=False,
         yticklabels=False,
         cmap="viridis",
+        vmin=0,
+        vmax=100,
         square=True,
     )
+    plt.tight_layout()
     if path is not None:
-        ax.figure.savefig(path)  # pyright: ignore
+        ax.figure.savefig(path, pad_inches=0)  # pyright: ignore
     ax.figure.clear()  # pyright: ignore
