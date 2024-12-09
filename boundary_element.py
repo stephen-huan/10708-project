0bb860038dbd617acf55553657f41f6891387142
Stephen Huan <stephen.huan@cgdct.moe>
2024-12-08 16:52:57 -0800

    refactor(boundary_element.py): simpler WoS api

--- boundary_element.py
+++ boundary_element.py
@@ -4,13 +4,8 @@ import jax
 from jax import random
 
 from elliptic.plotting import plot_mesh
-from elliptic.problems import (
-    boundary_function,
-    boundary_polylines,
-    get_matrix,
-    problems,
-)
-from elliptic.solvers import wos_domain, wos_sphere
+from elliptic.problems import get_matrix, problems
+from elliptic.solvers import wos
 
 # enable int64/float64
 jax.config.update("jax_enable_x64", True)
@@ -23,17 +18,8 @@ if __name__ == "__main__":
     for problem in problems:
         A, L, V, mesh = get_matrix(problem=problem, n=20, res=10)
         points = V.tabulate_dof_coordinates()
-        boundary_dirichlet = boundary_polylines(problem)
-        g = boundary_function(problem)
         rng, subkey = random.split(rng)
-        if problem != "circle":
-            values = wos_domain(
-                subkey, points, boundary_dirichlet, g, n_walks=100
-            )
-        else:
-            values = wos_sphere(
-                subkey, points, boundary_dirichlet, g, n_walks=100
-            )
+        values = wos(subkey, problem, points, n_walks=100)
 
         plot_mesh(
             problem, values, points, path=figures / f"{problem}_solution.png"
